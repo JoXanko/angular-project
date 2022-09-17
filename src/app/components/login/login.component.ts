0113a2045668fe5302f52e94648eb8fa48bf9bc7
JoXanko <jovan.vukadinovic25@gmail.com>
2022-09-17 13:05:12 +0200

    gif

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { MatCarousel, MatCarouselComponent } from 'ng-mat-carousel';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -21,18 +22,19 @@ export class LoginComponent implements OnInit {
   constructor(public auth: AuthService) {}
 
   ngOnInit(): void {
-    // this.hide=true;
-    this.slides[this.imageNum];
-    let paws = document.getElementById('gif');
-    let randX = (Math.random() * document.body.offsetHeight) | 0;
-    let randY = (Math.random() * document.body.offsetWidth) | 0;
-    if (paws != null) {
-      paws.style.top = randX + "px";
-      paws.style.left = randY + "px";
-    }
+    interval(1530)
+      .pipe(
+        tap(() => {
+          this.slides[this.imageNum];
+          let paws = document.getElementById('gif');
+          let randX = (Math.random() * document.body.offsetHeight) | 0;
+          let randY = (Math.random() * document.body.offsetWidth) | 0;
+          if (paws != null) {
+            paws.style.top = randX + 'px';
+            paws.style.left = randY + 'px';
+          }
+        })
+      )
+      .subscribe();
   }
-
-  // hideLogin(){//nece moz ovako zeza posle loggout
-  //   this.hide=!this.hide;
-  // }
 }
